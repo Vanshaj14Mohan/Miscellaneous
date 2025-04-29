6172d3ed8dd58dcd1b8f7381a2c43a09b7705db3
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-04-30 02:07:35 +0530

    Requests

---
+++ Backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.get("/register", (req,res)=>{
+    let {user, password} =req.query;
+    res.send(`Standard GET Response. Welcome ${user}`);
+});
+
+app.post("/register", (req,res)=>{
+    res.send("Standard POST Response");
+});
+//use hoppscotch to send request.
+
+app.listen(port, ()=>{
+    console.log(`App is listening on port ${port}`);
+});
