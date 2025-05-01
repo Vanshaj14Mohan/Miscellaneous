204efd99452d92832bdf1e5bbbdbe8b4ce8aaa44
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-05-01 22:49:49 +0530

    Requests

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+//app.use -> ie The work should be done for every request:
+app.use(express.urlencoded({extended: true}));//if urlencoded data happens that express automatically parse and understand it.
+app.use(express.json());//Just in case if we are sending data in json format. So it can parse it too.
+
 app.get("/register", (req,res)=>{
     let {user, password} =req.query;
     res.send(`Standard GET Response. Welcome ${user}`);
 });
 
 app.post("/register", (req,res)=>{
-    res.send("Standard POST Response");
+    console.log(req.body); //through req.body express will start understanding the data we sent
+    let {user, password} =req.body;
+    res.send(`Standard POST Response. Welcome ${user}`);
 });
 //use hoppscotch to send request and verify.
 
