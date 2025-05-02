1ef1e4cdb94cfc24cc2324dd687f9cd62372d3e4
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-05-02 18:55:10 +0530

    files

--- Frontend/app.js
+++ Frontend/app.js
@@ -4,3 +4,16 @@ arr.sayHello = () =>{
     console.log("Arr here");
 };
 
+function PersonMaker (name, age){
+    const person = {
+        name: name,
+        age: age,
+        talk(){
+            console.log(`Hi my name is ${this.name}`);
+        },
+    };
+    return person;
+}
+
+let p1 = PersonMaker("Adam", 25); //copy 
+let p2 = PersonMaker("Rose", 24); //copy
